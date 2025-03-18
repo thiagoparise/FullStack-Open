f819395e46d3b8198c5de4ec499986387d2489b5
Thiago Parise <thiagoparise1@gmail.com>
2025-03-18 11:19:05 -0300

    phonebook ex2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ handlePersonsFilter }) => <div>filter shown with <input onChange={handlePersonsFilter} /></div>
 
@@ -29,15 +30,20 @@ const Persons = ({ personsFiltered }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [personsFiltered, setPersonsFiltered] = useState(persons.slice()) 
+
+  const [persons, setPersons] = useState([])
+  const [personsFiltered, setPersonsFiltered] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+      setPersonsFiltered(response.data.slice())
+    })
+  }, [])
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
