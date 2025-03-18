7622f83ec2823eb097d884862881c7896c79f110
Thiago Parise <thiagoparise1@gmail.com>
2025-03-18 09:55:01 -0300

    phonebook step5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,38 +1,80 @@
 import { useState } from 'react'
 
+const Filter = ({ handlePersonsFilter }) => <div>filter shown with <input onChange={handlePersonsFilter} /></div>
+
+const PersonForm = ({ addNumber, handleNewNumber, handleNewName }) => {
+  return (
+    <form onSubmit={addNumber}>
+      <div>
+        name: <input onChange={handleNewName} />
+      </div>
+      <div>
+        number: <input onChange={handleNewNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form> 
+  ) 
+}
+
+const Persons = ({ personsFiltered }) => {
+  return (
+    <ul>
+        {
+          personsFiltered.map(person => <li key={person.name}>{person.name + ' ' + person.number}</li>)
+        }
+    </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { key: 'Arto Hellas', name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [personsFiltered, setPersonsFiltered] = useState(persons.slice()) 
   const [newName, setNewName] = useState('')
-  console.log(persons[0])
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleNewNumber = (event) => {
+  const handleNewName = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handlePersonsFilter = (event) => {
+    setPersonsFiltered(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
   }
 
   const addNumber = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName))
       alert(`${newName} is already added to the phonebook`)
-    else
-      setPersons(persons.concat({name: newName}))
+    else{
+      const personsUpdated = persons.concat({name: newName, number: newNumber, id: persons.length + 1})
+      setPersons(personsUpdated)
+      setPersonsFiltered(personsUpdated.slice())
+    }
   }
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
-        <div>
-          name: <input onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h1>Phonebook</h1>
+
+      <Filter handlePersonsFilter={handlePersonsFilter}/>
+
+      <h2>Add a new</h2>
+      
+      <PersonForm addNumber={addNumber} handleNewNumber={handleNewNumber} handleNewName={handleNewName} />
+
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
-      </ul>
+      
+      <Persons personsFiltered={personsFiltered} />
+
     </div>
   )
 }
